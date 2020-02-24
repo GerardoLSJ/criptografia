2282d32bea08e03c23e3b6e3ac6c7130f40741f7
Gerardo LSJ <gerardolsj@gmail.com>
2020-02-23 18:41:18 -0600

    unique key

--- UniqueCaesar.py
+++ UniqueCaesar.py
@@ -1,37 +1,33 @@
 
 
-# TODO 
-# DEFINE PROBLEM
-# WIP
-
-from collections import OrderedDict 
-
-def removeDupWithOrder(str): 
-    return "".join(OrderedDict.fromkeys(str)) 
-
 def UniqueCaesar(text,key,op): 
     """
     op: 0 = CIFRAR
     op: 1 = DESCIFRAR
     """
     LETTERS = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
-    LETTERS_KEY = removeDupWithOrder(key+LETTERS)
     result = "" 
     # Recorrer texto
     for i in range(len(text)): 
         char = text[i] 
         if (op == 0): 
             pos = LETTERS.find(char)
-            result += LETTERS_KEY[pos]
+            pos += LETTERS.find(key[i]) # posicion de llave
+            if(pos >= 27):
+                pos = pos % 27
+            result += LETTERS[pos]
         elif(op == 1):
-            pos = LETTERS_KEY.find(char)
+            pos = LETTERS.find(char)
+            pos -= LETTERS.find(key[i]) # posicion de llave
+            if(pos <= 0):
+                pos = pos % 27
             result += LETTERS[pos]
 
     return result 
   
 #Prueba
-mensaje = "HOLAMUNDOGERARDO"
-key = "GERARDO"
+mensaje = "AHOLADOTZ"
+key = "AGERARDOZ"
 print("Mensaje  : " + mensaje )
 cifrado = UniqueCaesar(mensaje,key,0)
 print("Cifrado: " + cifrado )
