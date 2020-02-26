0a35418c6571598548ed68d8cc4ba3c2f8bd6c3a
Gerardo LSJ <gerardolsj@gmail.com>
2020-02-25 22:38:14 -0600

    hill N

--- tarea5/HillCipher.py
+++ tarea5/HillCipher.py
@@ -1,82 +1,91 @@
 import numpy as np 
 from sympy import Matrix
 
+# Llave 
 keyMatrix = [[0] * 3 for i in range(3)] 
   
-# Generate vector for the message 
+# Mensaje
 messageVector = [[0] for i in range(3)] 
   
-# Generate vector for the cipher 
-cipherMatrix = [[0] for i in range(3)] 
+# Mensaje cifrado
+cipherMatrix = [] # [[0] for i in range(3)] 
   
-# Following function generates the 
-# key matrix for the key string 
+# GENERA LA MATRIZ LLAVE
 def getKeyMatrix(key): 
     k = 0
     for i in range(3): 
         for j in range(3): 
             keyMatrix[i][j] = ord(key[k]) % 65
             k += 1
-    # return keyMatrix
+    return keyMatrix
   
 # Following function encrypts the message 
 def encrypt(messageVector): 
-    for i in range(3): 
-        for j in range(1): 
-            cipherMatrix[i][j] = 0
-            for x in range(3): 
-                cipherMatrix[i][j] += (keyMatrix[i][x] * 
-                                       messageVector[x][j]) 
-            cipherMatrix[i][j] = cipherMatrix[i][j] % 26
-  
+    cipherMatrix = (Matrix(keyMatrix) * Matrix(messageVector)) % 26
+    return cipherMatrix.tolist()
+
 def HillCipher(message, key): 
   
-    # Get key matrix from the key string 
-    getKeyMatrix(key) 
-  
+    # GENERA  LA MATRIZ LLAVE GLOBAL 
+    keyMatrix = getKeyMatrix(key) 
+    #print(keyMatrix)
     # Generate vector for the message 
-    for i in range(3): 
-        messageVector[i][0] = ord(message[i]) % 65
+
+    if(len(message) % 3 != 0):
+        message += "X"
+        if(len(message) % 3 != 0):
+            message += "X"
+
+    message = list(message)
+    #print(message)
+    int_message = [ord(x) for x in message]
+    #print(int_message)
+    aux = np.array(int_message, dtype=int)
+    messageVector = np.reshape(aux,(3,-1)) % 65
+        #print(messageVector)
+
+    mensaje_cifrado = encrypt(messageVector) 
+    # print("cipherMatrix",mensaje_cifrado)
   
-    # Following function generates 
-    # the encrypted vector 
-    encrypt(messageVector) 
-  
-    # Generate the encrypted text  
-    # from the encrypted vector 
+    # Generar texto desde la matriz
     CipherText = [] 
-    for i in range(3): 
-        CipherText.append(chr(cipherMatrix[i][0] + 65)) 
-  
-    # Finally print the ciphertext 
-    # print("Ciphertext: ", "".join(CipherText)) 
-    return "".join(CipherText)
+    mensaje_cifrado = np.array(mensaje_cifrado, dtype=int)
+    # print("numpy", mensaje_cifrado)
+    flat = mensaje_cifrado.flatten()
+    for i in range(len(flat)): 
+        CipherText.append(chr(flat[i] + 65)) 
+
+    return {
+        "mensaje": "".join(CipherText),
+        "cifrado": mensaje_cifrado
+    }
   
 
-def HillDecipher(key):
+def HillDecipher(data, key):
     mat = Matrix(keyMatrix) # keyMatrix is your basic matrix ndrarray format
     inv = mat.inv_mod(26) #or any modulo you want
-
-    print(cipherMatrix)
-    res = np.dot(inv, cipherMatrix) 
-    print(res)
+    res = np.dot(inv, data["cifrado"]) % 26
+    flat = res.flatten()
+    #print("res",flat)
     DecipherText = [] 
-    for i in range(3): 
-        DecipherText.append(chr(res[i][0]%26 + 65)) 
+    for i in range(len(flat)): 
+        DecipherText.append(chr(flat[i] + 65)) 
   
-    return "".join(DecipherText)
+    return {
+        "mensaje": "".join(DecipherText),
+    }
 
 # Driver Code 
 
 # Get the message to  
 # be encrypted 
-mensaje = "ACT"
+mensaje = "PORMIRAZAHABLARAELESPIRITU"
 
 # Get the key 
 key = "GYBNQKURP"
 print("mensaje",mensaje)
-cifrado = HillCipher(mensaje, key) 
-print("cifrado",cifrado)
-
-print(HillDecipher(key))
+data = HillCipher(mensaje, key) 
+print("cifrado",data["mensaje"])
+descifrado = HillDecipher(data, key)
+print(descifrado)
 # descifrado = getKeyMatrix()
