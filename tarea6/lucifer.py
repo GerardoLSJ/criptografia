24a2da7198d9aaff4bd058746477afea75b17fc9
Marco Antonio Moreno <marcomorenoag@outlook.com>
2020-03-08 02:23:35 -0600

    Luciffer decryption ready

--- tarea6/lucifer.py
+++ tarea6/lucifer.py
@@ -3,83 +3,107 @@
 @authors: Luis Gerardo López S. Jiménez, Marco A. Moreno Guerra
 @date_creation: 06/03/2020
 @date_last_update: 08/03/2020
+@comments: 
+	To get more detailed process' information, uncomment print lines: 32, 36, 46, 50, 55, 71, 81.
+	There are some global constants at the beginning that facilite the change of values.
 '''
 
-# Global variable of the alphabet universe
+# Global variables
 LETTERS = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+PLAIN_TEXT = 'PORMIRAZAHABLARAELESPIRITU'
+BLOCK_SIZE = 8
+ROUNDS = 4
+SHIFT = 1
+
 
 # Functions
-def blockOperations(blocks, permute_order, shift):
+def blockOperations(blocks, permute_order, shift, process = 'encryption'):
 	final_result = []
 	for block in blocks:
 		block4 = [block[i:i+4] for i in range(0, len(block), 4)]
-		# Takes only left 4 char block
-		tmp = shiftLetters(block4[0], shift)
-		tmp = permutateLetters(tmp, permute_order)
-		final_result.append(crossBlocks(tmp, block4[1]))
-		print('PARTIAL RESULT: {}'.format(final_result))
+		if (process == 'encryption'):
+			partial_result1 = shiftLetters(block4[0], shift, process)
+			partial_result1 = permuteLetters(partial_result1, permute_order)
+			final_result.append(crossBlocks(partial_result1, block4[1]))
+		else:
+			partial_result1 = crossBlocks(block4[0], block4[1])
+			partial_result2 = permuteLetters(partial_result1[0:4], permute_order)
+			final_result.append(shiftLetters(partial_result2, shift, process) + partial_result1[4:8])
+		#print('PARTIAL RESULT: {}'.format(final_result))
 	return ''.join(final_result)
 
-def shiftLetters(block4, shift):
-	print("Before shift: ",block4)
-	newBlock4 = [0,0,0,0]
+def shiftLetters(block4, shift, operation = 'encryption'):
+	#print("Before shift: ",block4)
+	newBlock4 = [0, 0, 0, 0]
 	for i, char in enumerate(block4):
 		idx = LETTERS.find(char)
-		idx = (idx + 1) % 27
+		if (operation == 'encryption'):
+			idx = (idx + shift) % 27
+		else:
+			idx = (idx - shift) % 27
 		newBlock4[i] = LETTERS[idx]
 	result = ''.join(newBlock4)
-	print("After shift: ",result)
+	#print("After shift: ",result)
 	return result
 
-def permutateLetters(block4, permute_order):
-	print('Before permutate: {}'.format(block4))
-	newBlock4 = [0,0,0,0]
+def permuteLetters(block4, permute_order):
+	#print('Before permutate: {}'.format(block4))
+	newBlock4 = [0, 0, 0, 0]
 	for i, char in enumerate(block4):
 		newBlock4[i] = block4[permute_order[i]-1]
-		# 4213
 	result = ''.join(newBlock4)
-	print('After permutate: {}'.format(result))
+	#print('After permutate: {}'.format(result))
 	return result
 
 def crossBlocks(left_block, right_block):
 	return right_block + left_block
 
 def lucifer_encrypt(text, permute_order, shift, times = 4): # default times = 4
-	block_size = 8
-	mod = len(text) % block_size
-	# completar con caracter X si faltan letras en el bloque
+	mod = len(text) % BLOCK_SIZE
+	# Fills with X character if a block is not completed (8 chars)
 	if(mod != 0):
-		for i in range(0,block_size-mod):
-			text += "X"
+		for i in range(0,BLOCK_SIZE-mod):
+			text += 'X'
 
 	blocks = text
 
 	for i in range(0, times):
-		print('\nROUND {}:'.format(i + 1))
-		blocks = [blocks[i:i+block_size] for i in range(0, len(blocks), block_size)]
+		#print('\nENCRYPTION ROUND {}:'.format(i + 1))
+		blocks = [blocks[i:i+BLOCK_SIZE] for i in range(0, len(blocks), BLOCK_SIZE)]
 		blocks = blockOperations(blocks, permute_order, shift)
 
 	return blocks
 
-def lucifer_decrypt():
-	pass
+def lucifer_decrypt(text, permute_order, shift, times = 4): # default times = 4 (same that encryption)
+	blocks = text
+
+	for i in range(0, times):
+		#print('\nDECRYPTION ROUND {}:'.format(i + 1))
+		blocks = [blocks[i:i+BLOCK_SIZE] for i in range(0, len(blocks), BLOCK_SIZE)]
+		blocks = blockOperations(blocks, permute_order, shift, 'decryption')
+
+	return blocks
 
 def main(): 
-	# declaring text and key 
-	plain_text = 'MORENOGUERRALOPEZSANTIBAÑEZ'
-	permute_order = [4, 2, 1, 3]
-	shift = 1
-	rounds = 4
+	# ENCRYPTION 
+	permute_order_encrypt = [4, 2, 1, 3]
+	lucifer_encrypted_text = lucifer_encrypt(PLAIN_TEXT, permute_order_encrypt, SHIFT, ROUNDS) 
+	print('Encrypt permute order: {}'.format(permute_order_encrypt))
 
-	# calling encryption function 
-	lucifer_encrypted_text = lucifer_encrypt(plain_text, permute_order, shift, rounds) 
+	# DECRYPTION 
+	permute_order_decrypt = [3, 2, 4, 1]
+	print('Decrypt permute order: {}'.format(permute_order_decrypt))
 
-	print('Plaint text: {}'.format(plain_text))
-	print('Encrypted text: {}'.format(lucifer_encrypted_text)) 
-
-	# calling decryption function 
-	#print('Decrypted Text: {}'.format( lucifer_decrypt(lucifer_encrypted_text, a, b) )) 
-
+	# RESULTS (SUMMARY)
+	print('\nPlaint text: {}'.format(PLAIN_TEXT))
+	print('Encrypted text: {}'.format(lucifer_encrypted_text))
+	print('Decrypted Text: {}'.format(lucifer_decrypt(lucifer_encrypted_text, permute_order_decrypt, SHIFT, ROUNDS))) 
+	'''
+	hola mund
+	ipmb mund
+	bpim mund
+	mund bpim
+	'''
 
 # Code execution
 if __name__ == '__main__': 
